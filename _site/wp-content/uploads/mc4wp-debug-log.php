210da116715309eb0912d1bb432f14ba6f965c50
Sid Krishnan <krishnan.sid@gmail.com>
2020-04-09 13:18:42 -0400

    try building everything

--- _site/wp-content/uploads/mc4wp-debug-log.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-[2016-02-01 07:05:48] WARNING: Form 129 > bloomberg490@gmail.com is already subscribed to the selected list(s)
-03:50] WARNING: Form 129 > Chadheinen13@yahoo.com is already subscribed to the selected list(s)
-[2016-07-07 05:34:40] WARNING: Form 129 > ***************sandboxhot.com is already subscribed to the selected list(s)
